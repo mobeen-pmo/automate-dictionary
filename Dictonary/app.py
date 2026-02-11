ea3f8743a4f57484f118562fa4bf50931ad74dab
Mirza  M Mobeen <148184307+mobeen-pmo@users.noreply.github.com>
2026-02-11 15:44:29 +0500

    Restore initial comments and page configuration

--- Dictonary/app.py
+++ Dictonary/app.py
@@ -440,7 +440,8 @@ else:
 # ──────────────────────────────────────────────
 st.markdown(
     '<div class="custom-footer">'
-    "© 2025 | Developed by <span>Mirza Muhammad Mobeen</span>"
+    "© 2026 | Developed by <span>Mirza Muhammad Mobeen</span>"
     "</div>",
     unsafe_allow_html=True,
 )
+
